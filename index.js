59b2881b644b14756675c8d1aeaf055dc489a085
Samuel Rodriguez <samrodriguez826@gmail.com>
2023-05-29 21:53:19 -0400

    added tooltip to input field for clarity of use

--- index.js
+++ index.js
@@ -165,3 +165,6 @@ calc.setExpression({
 $('#keySelected').text("");
 
 }, false);
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
